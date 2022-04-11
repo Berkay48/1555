1e02301bdff7007e873225c0767f9123d6567a61
Berkay48 <68476466+Berkay48@users.noreply.github.com>
2022-04-11 12:06:15 +0300

    Add files via upload

---
+++ matthe.js
@@ -0,0 +1,77 @@
+// Sorununz olursa Matthe#0001 ulaşınız. https://discord.gg/vAKkqEqgfE
+
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const ayarlar = require("./ayarlar.json")
+const moment = require("moment")//Matthe#0001
+const fs = require("fs")
+const db = require("quick.db")
+const chalk = require("chalk")
+require('./util/Loader.js')(client)
+
+client.commands = new Discord.Collection()
+client.aliases = new Discord.Collection()
+fs.readdir('./commands/', (err, files) => { 
+  if (err) console.error(err);               
+  console.log(`${files.length} komut yüklenecek.`)//Youtube Matthe
+  files.forEach(f => {                    
+    let props = require(`./commands/${f}`)
+    console.log(`${props.config.name} komutu yüklendi.`)
+    client.commands.set(props.config.name, props)
+    props.config.aliases.forEach(alias => {       
+      client.aliases.set(alias, props.config.name)
+    });
+  });
+})
+
+client.on("ready", () => {
+    console.log(chalk.redBright(`Bénz Bot Aktif!`))
+})
+
+// BOTUN İNTENTLERİNİ AÇMAYI UNUTMAYIN 
+client.login(process.env.TOKEN)
+
+client.on("ready", () => {
+  client.channels.cache.get(ayarlar.botSesKanal).join();
+  });
+//Youtube Matthe
+//----------------------------------------------------- TAG ROL ------------------------------------------------\\
+
+// tag rol kodu bana ait değildir, geliştirip sizlere sundum.
+client.on("userUpdate", async function(oldUser, newUser) { 
+    const guildID = (ayarlar.SunucuID)
+    const roleID = (ayarlar.tagRol)
+    const tag = (ayarlar.tag)
+    const etikettag = (ayarlar.etikettag)
+    const chat = (ayarlar.sohbetKanal)
+    const taglog = (ayarlar.tagLog)
+  
+    
+    const guild = client.guilds.cache.get(guildID)
+    const role = guild.roles.cache.find(roleInfo => roleInfo.id === roleID)
+    const member = guild.members.cache.get(newUser.id)
+    const embed = new Discord.MessageEmbed().setAuthor(member.displayName, member.user.avatarURL({ dynamic: true })).setColor('RANDOM').setTimestamp().setFooter('Bénz was here!');
+    if (newUser.username !== oldUser.username) {
+        if (oldUser.username.includes(tag) && !newUser.username.includes(tag)) {
+            member.roles.remove(roleID)
+            client.channels.cache.get(taglog).send(embed.setDescription(`${newUser} Kullanıcısı tagımızı çıkardığı için taglı rolü alındı!`))
+        } else if (!oldUser.username.includes(tag) && newUser.username.includes(tag)) {
+            member.roles.add(roleID)
+            client.channels.cache.get(chat).send(`**${newUser} Tagımızı alarak ailemize katıldı!**`)
+            client.channels.cache.get(taglog).send(embed.setDescription(`${newUser} Kullanıcısı tagımızı aldığı için taglı rolü verildi!`))
+        }
+    }
+   if (newUser.discriminator !== oldUser.discriminator) {
+        if (oldUser.discriminator == (ayarlar.etikettag) && newUser.discriminator !== (ayarlar.etikettag)) {
+            member.roles.remove(roleID)
+            client.channels.cache.get(taglog).send(embed.setDescription(`${newUser} Kullanıcısı etiket tagımızı çıkardığı için taglı rolü alındı!`))
+        } else if (oldUser.discriminator !== (ayarlar.etikettag) && newUser.discriminator == (ayarlar.etikettag)) {
+            member.roles.add(roleID)-
+            client.channels.cache.get(taglog).send(embed.setDescription(`${newUser} Kullanıcısı etiket tagımızı aldığı için taglı rolü verildi!`))
+            client.channels.cache.get(chat).send(`**${newUser} Etiket tagımızı alarak ailemize katıldı!**`)
+        }
+    }
+  
+  })
+
+//----------------------------------------------------- TAG ROL ------------------------------------------------\\
